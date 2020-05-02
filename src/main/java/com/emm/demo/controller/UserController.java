dfee8f291106ac67533a84678a7cf05b3db1a463
Moila, Emmanuel <EMoila@fnb.co.za>
2020-05-02 12:45:45 +0200

    added home endpoint

--- src/main/java/com/emm/demo/controller/UserController.java
+++ src/main/java/com/emm/demo/controller/UserController.java
@@ -3,13 +3,25 @@ package com.emm.demo.controller;
 import com.emm.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
+
+    private List<User> userList = new ArrayList<>();
+
+    @GetMapping("/")
+    public String loadHomePage(){
+        return "Home Page - Demo \n try /user endpoint ";
+    }
 
     @GetMapping("/user")
     public User getUser(){
         return new User("Emmanuel","Moila","eppy@gts.com");
     }
+
 }
